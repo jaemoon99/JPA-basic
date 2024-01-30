8c12fefadbd96bac1a04be41d75e1e0c0cc07e66
jaemoon <woans5970@naver.com>
2024-01-31 00:23:19 +0900

    section 4: 기본키 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,35 +16,11 @@ public class JpaMain {
 
         //code
         try {
-            //생성
-            //비영속
-//            Member member = new Member(100L, "A");
-//
-//            //영속(영속성 컨텍스트에 정보가 저정됨)
-//            em.persist(member);
+            Member member = new Member();
+            member.setUsername("C");
 
-            //전체 조회
-//            Member findMember = em.find(Member.class, 1L);
-//            System.out.println("findMember.id = " + findMember.getId());
-//            System.out.println("findMember.name = " + findMember.getName());
+            em.persist(member);
 
-            //조건 조회
-//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-//                    .setFirstResult(1) //시작 번호부터
-//                    .setMaxResults(1) //끝 번호까지
-//                    .getResultList(); //JPQL
-
-//            for (Member member : result) {
-//                System.out.println("member.name = " + member.getName());
-//            }
-
-            //삭제
-//            em.remove(findMember);
-
-            //수정
-//            findMember.setName("HelloJPA");
-
-            //컨텍스트에 있는 정보를 디비에 저장
             tx.commit(); //트랜잭션 커밋
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -5,21 +5,25 @@ import jakarta.persistence.*;
 import java.util.Date;
 
 @Entity
+//@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
+@TableGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        table = "MY_SEQUENCES",
+        pkColumnValue = "MEMBER_SEQ",
+        allocationSize = 1
+)
 public class Member {
 
     @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
+    @GeneratedValue(
+            strategy = GenerationType.TABLE,
+            generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
+
     @Column(name = "name")
     private String username;
-    private Integer age;
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-    @Lob
-    private String description;
 
     public Member() {
     }
@@ -38,45 +42,5 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 }
