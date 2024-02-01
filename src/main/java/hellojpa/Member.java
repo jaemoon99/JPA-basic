1b2abf8342496651dcc44b937e82548c136ea928
jaemoon <woans5970@naver.com>
2024-02-01 23:33:08 +0900

    section 6: N : M

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,6 +2,9 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Member {
 
@@ -27,6 +30,11 @@ public class Member {
     @OneToOne
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
+
+//    @ManyToMany
+//    @JoinTable(name = "MEMBER_PRODUCT")
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
 
     public Long getId() {
         return id;
