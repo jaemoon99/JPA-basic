877cc9a5e4f2581e274cb7816f8197f4680ee050
jaemoon <woans5970@naver.com>
2024-02-04 17:24:01 +0900

    section 10: JPQL

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,33 +17,12 @@ public class JpaMain {
 
         //code
         try {
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity", "street", "100"));
+            List<Member> result = em.createQuery(
+                    "select m from Member as m where m.username like '%kim%'", Member.class).getResultList();
 
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("피자");
-            member.getFavoriteFoods().add("족발");
-
-            member.getAddressHistory().add(new Address("old1", "street", "100"));
-            member.getAddressHistory().add(new Address("old2", "street", "100"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("==========START==========");
-            Member findMember = em.find(Member.class, member.getId());
-
-            Address a = findMember.getHomeAddress();
-            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
-
-            findMember.getFavoriteFoods().remove("치킨");
-            findMember.getFavoriteFoods().add("한식");
-
-            findMember.getAddressHistory().remove(new Address("old1", "street", "100"));
-            findMember.getAddressHistory().add(new Address("newCity1", "street", "100"));
+            for (Member member : result) {
+                System.out.println("member = " + member);
+            }
 
             tx.commit(); //트랜잭션 커밋
         } catch (Exception e) {
